6af1823e69f8e9737a561276edd59040d05509b7
王志远 <w1968953844@163.com>
2020-07-29 19:37:37 +0800

    ftpclient样板

---
+++ ftpclient/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace ftpclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "39.106.178.220";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(292, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(65, 25);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "21";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(459, 18);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 25);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "ftpuser";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(730, 18);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PasswordChar = '*';
+            this.textBox4.Size = new System.Drawing.Size(180, 25);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "wang1234";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(928, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(374, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "UserName:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(645, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "本地:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 449);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "状态:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(15, 481);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(1033, 169);
+            this.listBox1.TabIndex = 12;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(15, 127);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(469, 304);
+            this.listBox2.TabIndex = 13;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(548, 127);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(500, 304);
+            this.listBox3.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(394, 441);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "上传";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(880, 441);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "下载";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(63, 75);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(421, 25);
+            this.textBox5.TabIndex = 17;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(501, 75);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(973, 441);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 34);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "删除";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(928, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(120, 36);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "显示密码";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 669);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ ftpclient/Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ftpclient
+{
+    public partial class Form1 : Form
+    {
+
+        #region  Private variable
+        private TcpClient cmdServer;
+        private TcpClient dataServer;
+        private NetworkStream cmdStrmWtr;
+        private StreamReader cmdStrmRdr;
+        private NetworkStream dataStrmWtr;
+        private StreamReader dataStrmRdr;
+        private String cmdData;
+        private byte[] szData;
+        private const String CRLF = "\r\n";
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //连接
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "连接")
+            {
+                
+                Cursor cr = Cursor.Current;
+                Cursor.Current = Cursors.WaitCursor;
+                cmdServer = new TcpClient(textBox1.Text, Convert.ToInt32(textBox2.Text));
+                listBox1.Items.Clear();
+                try
+                {
+                    cmdStrmRdr = new StreamReader(cmdServer.GetStream());
+                    cmdStrmWtr = cmdServer.GetStream();
+                    this.getSatus();
+
+                    string retstr;
+
+                    //Login
+                    cmdData = "USER " + textBox3.Text + CRLF;
+                    szData = System.Text.Encoding.ASCII.GetBytes(cmdData.ToCharArray());
+                    cmdStrmWtr.Write(szData, 0, szData.Length);
+                    this.getSatus();
+
+                    cmdData = "PASS " + textBox4.Text + CRLF;
+                    szData = System.Text.Encoding.ASCII.GetBytes(cmdData.ToCharArray());
+                    cmdStrmWtr.Write(szData, 0, szData.Length);
+                    retstr = this.getSatus().Substring(0, 3);
+                    if (Convert.ToInt32(retstr) == 530) throw new InvalidOperationException("帐号密码错误");
+
+                    this.freshFileBox_Right();
+
+                    label1.Text = textBox1.Text + ":";
+                    button1.Text = "断开";
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    button5.Enabled = true;
+                }
+                catch (InvalidOperationException err)
+                {
+                    listBox1.Items.Add("ERROR: " + err.Message.ToString());
+                }
+                finally
+                {
+                    Cursor.Current = cr;
+                }
+            }
+            else
+            {
+                Cursor cr = Cursor.Current;
+                Cursor.Current = Cursors.WaitCursor;
+
+                //Logout
+
+                cmdData = "QUIT" + CRLF;
+                szData = System.Text.Encoding.ASCII.GetBytes(cmdData.ToCharArray());
+                cmdStrmWtr.Write(szData, 0, szData.Length);
+                this.getSatus();
+
+
+                cmdStrmWtr.Close();
+                cmdStrmRdr.Close();
+
+                label1.Text = "";
+                button1.Text = "连接";
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button5.Enabled = false;
+                listBox3.Items.Clear();
+
+                Cursor.Current = cr;
+            }
+        }
+
+        #region  Private Functions
+
+        /// <summary>
+        /// 获取命令端口返回结果，并记录在listBox1上
+        /// </summary>
+        private String getSatus()
+        {
+
+            String ret = cmdStrmRdr.ReadLine();
+            listBox1.Items.Add(ret);
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            return ret;
+        }
+
+        /// <summary>
+        /// 进入被动模式，并初始化数据端口的输入输出流
+        /// </summary>
+        private void openDataPort()
+        {
+            string retstr;
+            string[] retArray;
+            int dataPort;
+
+            // Start Passive Mode 
+            cmdData = "PASV" + CRLF;
+            szData = System.Text.Encoding.ASCII.GetBytes(cmdData.ToCharArray());
+            cmdStrmWtr.Write(szData, 0, szData.Length);
+            retstr = this.getSatus();
+            
+            // Calculate data's port
+            retArray = Regex.Split(retstr, ",");
+            retstr = retArray[5].Substring(0, retArray[5].Length-2);
+            dataPort = Convert.ToInt32(retArray[4]) * 256 + Convert.ToInt32(retstr);
+            listBox1.Items.Add("Get dataPort=" + dataPort);
+
+
+            //Connect to the dataPort
+            dataServer = new TcpClient(textBox1.Text, dataPort);
+            dataStrmRdr = new StreamReader(dataServer.GetStream());
+            dataStrmWtr = dataServer.GetStream();
+        }
+
+        /// <summary>
+        /// 断开数据端口的连接
+        /// </summary>
+        private void closeDataPort()
+        {
+            dataStrmRdr.Close();
+            dataStrmWtr.Close();
+            this.getSatus();
+
+            cmdData = "ABOR" + CRLF;
+            szData = System.Text.Encoding.ASCII.GetBytes(cmdData.ToCharArray());
+            cmdStrmWtr.Write(szData, 0, szData.Length);
+            this.getSatus();
+
+        }
+
+        /// <summary>
+        /// 获得/刷新 右侧的服务器文件列表
+        /// </summary>
+        private void freshFileBox_Right()
+        {
+
+            openDataPort();
+
+            string absFilePath;
+
+            //List
+            cmdData = "LIST" + CRLF;
+            szData = System.Text.Encoding.ASCII.GetBytes(cmdData.ToCharArray());
+            cmdStrmWtr.Write(szData, 0, szData.Length);
+            this.getSatus();
+
+            listBox3.Items.Clear();
+            while ((absFilePath = dataStrmRdr.ReadLine()) != null)
+            {
+                string[] temp = Regex.Split(absFilePath, " ");
+                listBox3.Items.Add(temp[temp.Length - 1]);
+            }
+
+            closeDataPort();
+        }
+
+        /// <summary>
+        /// 获得/刷新 左侧的本地文件列表
+        /// </summary>
+        private void freshFileBox_Left()
+        {
+            listBox2.Items.Clear();
+            if (textBox5.Text == "") return;
+            var files = Directory.GetFiles(textBox5.Text, "*.*");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+                string[] temp = Regex.Split(file, @"\\");
+                listBox2.Items.Add(temp[temp.Length - 1]);
+            }
+        }
+
+        #endregion
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string path = string.Empty;
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+                listBox1.Items.Add("选中本地路径:" + path);
+            }
+
+            textBox5.Text = path;
+            freshFileBox_Left();
+        }
+
+        //上传文件
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox5.Text == "" || listBox2.SelectedIndex < 0)
+            {
+                MessageBox.Show("请选择上传的文件", "ERROR");
+                return;
+            }
+
+            Cursor cr = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+
+            string fileName = listBox2.Items[listBox2.SelectedIndex].ToString();
+            string filePath = textBox5.Text + "\\" + fileName;
+
+            this.openDataPort();
+
+            cmdData = "STOR " + fileName + CRLF;
+            char[] a = cmdData.ToCharArray();
+            byte[] b = System.Text.Encoding.UTF8.GetBytes(a);
+            //szData = System.Text.Encoding.ASCII.GetBytes(a);
+            szData = b;
+            cmdStrmWtr.Write(szData, 0, szData.Length);
+            this.getSatus();
+
+            FileStream fstrm = new FileStream(filePath, FileMode.Open);
+            byte[] fbytes = new byte[1030];
+            int cnt = 0;
+            while ((cnt = fstrm.Read(fbytes, 0, 1024)) > 0)
+            {
+                dataStrmWtr.Write(fbytes, 0, cnt);
+            }
+            fstrm.Close();
+
+            this.closeDataPort();
+
+            this.freshFileBox_Right();
+
+            Cursor.Current = cr;
+        }
+
+        //下载文件
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox5.Text == "" || listBox3.SelectedIndex < 0)
+            {
+                MessageBox.Show("请选择目标文件和下载路径", "ERROR");
+                return;
+            }
+
+            Cursor cr = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+
+            string fileName = listBox3.Items[listBox3.SelectedIndex].ToString();
+            string filePath = textBox5.Text + "\\" + fileName;
+
+            this.openDataPort();
+
+            cmdData = "RETR " + fileName + CRLF;
+            //szData = System.Text.Encoding.ASCII.GetBytes(cmdData.ToCharArray());
+            szData = System.Text.Encoding.UTF8.GetBytes(cmdData.ToCharArray());
+            cmdStrmWtr.Write(szData, 0, szData.Length);
+            this.getSatus();
+
+            FileStream fstrm = new FileStream(filePath, FileMode.OpenOrCreate);
+            char[] fchars = new char[1030];
+            byte[] fbytes = new byte[1030];
+            int cnt = 0;
+            while ((cnt = dataStrmWtr.Read(fbytes, 0, 1024)) > 0)
+            {
+                fstrm.Write(fbytes, 0, cnt);
+            }
+            fstrm.Close();
+
+            this.closeDataPort();
+
+            this.freshFileBox_Left();
+
+            Cursor.Current = cr;
+        }
+
+        //删除文件
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listBox3.SelectedIndex < 0)
+            {
+                MessageBox.Show("请选择要删除的目标文件", "ERROR");
+                return;
+            }
+
+            Cursor cr = Cursor.Current;
+            Cursor.Current = Cursors.WaitCursor;
+
+            string fileName = listBox3.Items[listBox3.SelectedIndex].ToString();
+            string filePath = textBox5.Text + "\\" + fileName;
+
+            cmdData = "DELE " + fileName + CRLF;
+            szData = System.Text.Encoding.UTF8.GetBytes(cmdData.ToCharArray());
+            cmdStrmWtr.Write(szData, 0, szData.Length);
+            this.getSatus();
+            
+            this.freshFileBox_Right();
+
+            Cursor.Current = cr;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == "显示密码")
+            {
+                textBox4.PasswordChar = '\0';
+                button6.Text = "隐藏密码";
+            }
+            else
+            {
+                textBox4.PasswordChar = '*';
+                button6.Text = "显示密码";
+            }
+        }
+    }
+}
